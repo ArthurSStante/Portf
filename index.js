b3fd20a5e8617efac08f8be73bb7508f1dd8beec
ArthurSStante <arthursaltori@gmail.com>
2023-03-09 17:24:12 -0300

    Portf

---
+++ index.js
@@ -0,0 +1,4 @@
+// function m(){
+//     type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+//     nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+// }
